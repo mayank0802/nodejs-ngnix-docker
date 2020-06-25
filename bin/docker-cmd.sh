29b9f08090c00268574d58b1c72d141d20218cdf
AditiMahajan <55207725+Adi3012Mhjn@users.noreply.github.com>
2020-06-25 16:55:40 +0530

    version 14 new

---
+++ bin/docker-cmd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+NODE_ENV="${NODE_ENV:-development}"
+SCRIPT_HOME=`dirname $0`
+PROJECT_HOME="${SCRIPT_HOME}/.."
+
+# build the app
+yarn run build
+
+# copy the dist to nginx html dir
+cp -a "${PROJECT_HOME}/dist/." /usr/share/nginx/html/
+
+# update nginx default.conf
+cp "${PROJECT_HOME}/config/nginx.default.conf" /etc/nginx/conf.d/default.conf
+
+# start nginx
+nginx -g 'daemon off;'
