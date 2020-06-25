a9a89d7aa02c029521000949f306c1e3c1a08ed2
AditiMahajan <55207725+Adi3012Mhjn@users.noreply.github.com>
2020-06-25 14:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(8080,()=>{
-    console.log("Listening on port 8080")
+app.listen(80,()=>{
+    console.log("Listening on port 80")
 });
